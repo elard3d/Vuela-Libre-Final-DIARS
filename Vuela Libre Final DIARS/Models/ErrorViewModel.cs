37494ae9192ae0c2a1e66ab0d77727dadd7f6757
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-25 18:41:48 -0500

    Agregar archivos de proyecto.

---
+++ Vuela Libre Final DIARS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vuela_Libre_Final_DIARS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
